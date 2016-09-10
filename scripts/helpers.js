7918e18745ef568ff70b134e12c22d979446b504
chenwanxing <chenwanxing@wilddog.com>
2016-09-10 17:51:50 +0800

    fix ui.js wrong code

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -66,22 +66,23 @@ hexo.extend.helper.register('doc_sidebar', function(className){
         subList = subList.replace('sublist', 'sublist current');
         itemClass += ' current';
       }
-      var thirList = '<ul class=\'sublist\'>';
       if (typeof link === 'object') {
-        subList += '<li class=\'sublist-item\'><strong class="' + className + '-title">' + text + '</strong>';
+        var thirList = '<ul class=\'sublist\'>';
+        var thirListStart = '<li class=\'sublist-item\'><strong class="' + className + '-title">' + text + '</strong>';
         _.each(link, function (url, content) {
           var currentClass = itemClass.replace(' current', '');
           if (url === path) {
             listStart = listStart.replace('-title', '-title current select');
             subList = subList.replace('sublist', 'sublist current');
-            subList = subList.replace('-title', '-title select');
+            thirListStart = thirListStart.replace('-title', '-title select');
             thirList = thirList.replace('sublist', 'sublist current');
             currentClass += ' current';
           }
           thirList += '<li class=\'sublist-item\'><a href="' + url + '" class="' + currentClass + '">' + (content) + '</a><\/li>';
         })
-        thirList += '<\/ul>'
-        subList += thirList;
+        thirList += '<\/ul>';
+        thirListStart += (thirList + '<\/li>');
+        subList += thirListStart;
         subList += '<\/li>';
       } else {
         subList += '<li class=\'sublist-item\'><a href="' + link + '" class="' + itemClass + '">' + (text) + '</a>';
