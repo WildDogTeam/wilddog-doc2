eb5beed52b1d9f4564e2235bab533f2cf9f99f42
chenwanxing <chenwanxing@wilddog.com>
2016-11-24 19:34:42 +0800

    test search

---
+++ themes/navy/source/js/search.js
@@ -0,0 +1,26 @@
+//搜索功能
+var searchbar = getClass('searchbar')[0];
+var mainNav = document.getElementById('main-nav');
+var closeSearch = getClass('close-search')[0];
+var searchInput = getClass('searchbar-inputting')[0];
+searchbar.addEventListener('click', function () {
+  addClass(searchInput, 'searchShow')
+  addClass(mainNav, 'fadeHide')
+  addClass(userProfile, 'fadeHide')
+  addClass(loginTab, 'fadeHide')
+  addClass(this, 'fadeHide');
+});
+closeSearch.addEventListener('click', function (e) {
+  e.stopPropagation();
+  removeClass(searchInput, 'searchShow');
+  removeClass(mainNav, 'fadeHide');
+  removeClass(userProfile, 'fadeHide');
+  removeClass(loginTab, 'fadeHide');
+  removeClass(searchbar, 'fadeHide');
+})
+
+getClass('search-input')[0].addEventListener('keydown', function (e) {
+  if (e.keyCode === 13) {
+    window.location.href = '/search/index.html?keyword=' + encodeURIComponent(this.value.split('').slice(0, 19).join(''));
+  }
+})
